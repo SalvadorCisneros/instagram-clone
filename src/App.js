c9099b919b4741ad2f371eac0fcf9e31e0a22b96
Salvador Cisneros <77074008+SalvadorCisneros@users.noreply.github.com>
2024-04-30 13:10:47 -0600

    user authentication features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
+ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post from './components/post/post';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, signInWithEmailAndPassword} from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { collection, onSnapshot } from "firebase/firestore";
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 
 
 function App() {
   const [posts, setPosts] = useState([]); 
+  const [openSignUp, setOpenSignUp] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState(false);
+
+
   
 
   const firebaseConfig = {
@@ -29,19 +40,123 @@ function App() {
   const postsCollection = collection(db, 'post');
 
   useEffect(() => {
-      onSnapshot(postsCollection, (snapshot) => {
-          setPosts(snapshot.docs.map(doc => doc.data()));
-      });
-  }, []);
+    onSnapshot(postsCollection, (snapshot) => {
+        setPosts(snapshot.docs.map(doc => ({
+            id: doc.id,
+            post: doc.data()
+        })));  
+    });
+}, []);
+
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    createUserWithEmailAndPassword(auth,email, password)
+    .catch((error) => alert(error.message));
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, password, email)
+    .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  };
+
+  useEffect(() => {
+  onAuthStateChanged(auth,(authUser) => {
+    if (authUser){
+      console.log(authUser);
+      setUser(authUser);
+
+      if (authUser.displayName){
+
+      }  else{
+        // Check if authUser is defined before updating profile
+        if (authUser) {
+          return updateProfile(authUser, {
+            displayName: username,
+          });
+        }
+      }
+    } else{
+      setUser(null);
+    }
+  })
+}, [user, username]);
+
 
   return (
     <div className="App">
-      {posts.map( post => (
-        <Post image={post.image} description={post.description} user={post.user} />
-      ))
+      <Button onClick={signIn}>Sign In</Button>
+      <Modal 
+      open={openSignIn} 
+      onClose={() => setOpenSignIn(false)}>
+        <div className='signInContainer'>
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          
+        />
+          <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        </div>
 
-      }
+      </Modal>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Log Out</Button>
+      ): (
+        <Button onClick={signUp}>Sign Up</Button>
+      )}
+      <Modal
+        open={openSignUp}
+        onClose={() => setOpenSignUp(false)}
+        
+      >
+        <div className='modalContainer'>
+        <TextField
+          required
+          id="outlined-required"
+          label="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+         
+        />
+          <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          
+        />
+          <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+          <Button type='submit' onClick={signUp}>Register</Button>
+        </div>
+      </Modal>
       
+      {posts.map(post => (
+        <Post key={post.id} id={post.id} post={post.post} />
+      ))}
     </div>
   );
 }
--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from 'react';
 import "./post.css";
 import Avatar from '@mui/material/Avatar';
 
-export default function Post({image, description, user}) {
+export default function Post({ id, post }) {
+  const { image, description, user } = post;
+
   return (
     <div className='postContainer'>
-    
-        <div className='userContainer'>
-            <Avatar>{user.charAt(0)}</Avatar>
-            <h2>{user}</h2>
-        </div>
-        
-        
-        <img src={image} alt="" />
-        <div className='descriptionContainer'>
-            <h4>{user}</h4>
-            <p>{description}</p>
-        </div>
-        
-
+      <div className='userContainer'>
+        <Avatar>{user.charAt(0)}</Avatar>
+        <h2>{user}</h2>
+      </div>
+      <img src={image} alt="" />
+      <div className='descriptionContainer'>
+        <h4>{user}</h4>
+        <p>{description}</p>
+      </div>
     </div>
-  )
+  );
 }
